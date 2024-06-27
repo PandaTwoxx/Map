159afb78ae1a32b174e0d84681b03ee59d66edba
Weston Gorczyca <weston.gorczyca@gmail.com>
2024-06-27 10:38:00 -0400

    chore: Added login and home page

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-# Flask app goes here
+from flask import Flask, render_template
+import uuid
+
+app = Flask(__name__)
+app.config(uuid.uuid4().hex)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
