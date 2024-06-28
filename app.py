eb7049a4030b6619195834e2b329410e64cc1327
Weston Gorczyca <weston.gorczyca@gmail.com>
2024-06-28 08:40:41 -0400

    chore: Added bootstrap and signup page

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template
+from waitress import serve
 import uuid
 
 app = Flask(__name__)
-app.config(uuid.uuid4().hex)
+app.config['SECRET_KEY'] = uuid.uuid4().hex
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    serve(app, host='0.0.0.0', port=8080)
