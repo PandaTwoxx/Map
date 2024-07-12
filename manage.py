5b16f38e3b7144d04f9bcbc130dc78acc905c894
Weston Gorczyca <weston.gorczyca@gmail.com>
2024-07-12 07:21:57 -0400

    Added way to clear cache

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from service.runtime import run
+from service.runtime import run, clear_cache
 
 def start():
     catch = input("Server status: Idle (Press S + Enter to start or any other key to abort)")
@@ -15,6 +15,9 @@ if __name__ == '__main__':
         if command == 'exit':
             print('Abort: Ending session')
             break
+        if command == 'clear-cache':
+            print('Management console: Clearing save data')
+            clear_cache()
         if command == 'help':
             print('Help')
             print('exit: Abort')
--- service/runtime.py
+++ service/runtime.py
@@ -1,5 +1,15 @@
 from service.__init__ import beginServer
+import shutil
+
+
+
 def run():
     # Begin the server here
     print('Starting server')
     beginServer()
+
+
+
+def clear_cache():
+    dir_path = 'Session_Logs/'
+    shutil.rmtree(dir_path)
