d9ad2a54e7923beab12ba6978d9d19441ad5c9da
Weston Gorczyca <weston.gorczyca@gmail.com>
2024-09-29 13:14:09 -0400

    removed useless stuff

--- manage.py
+++ manage.py
@@ -1,21 +1,6 @@
-"""Used for reading args"""
-import sys
+"""Starts app"""
 
 import service
 
 if __name__ == '__main__':
-    if 'run' in sys.argv:
-        print('Management console: Started Server')
-        service.run()
-    while True:
-        command = input('Enter command: ')
-        if command == 'run':
-            print('Management console: Started Server')
-            service.run()
-        if command == 'exit':
-            print('Abort: Ending session')
-            break
-        if command == 'help':
-            print('Help')
-            print('exit: Abort')
-            print('run: Run server')
+    service.run()
